87980247a459c8dddffbfb07e8b3273d08634a52
FalcoLluc <joan.lluc.fernandez@estudiantat.upc.edu>
2024-09-21 18:39:00 +0200

    Versio_Definitiva

--- RoleNames.java
+++
@@ -1,7 +0,0 @@
-public interface RoleNames {
-    public final static String ADMINISTRATOR = "Administrator";
-    public final static String PROGRAMMER = "Programmer";
-    public final static String MANAGER = "Manager";
-
-
-}
